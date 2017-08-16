55e5a973456644400b3d8a3cf1945ee4117242e0
barsuk <barsuk@rg.ru>
2017-08-16 19:27:50 +0300

    add: migration to define zone by parsed region from front
    Special changes are inside database content of Area table.
    Look scheme at https://goo.gl/U7A9eB
    https://www.wrike.com/open.htm?id=154832922

---
+++ www/app/DoctrineMigrations/Version20170816095240.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170816095240 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE area ADD works_id VARCHAR(255) NOT NULL, ADD `link` VARCHAR(255) NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE area DROP works_id, DROP `link`');
+    }
+}
