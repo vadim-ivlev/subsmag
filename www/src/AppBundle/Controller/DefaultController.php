f0bb3bce6a16933e3e30b743c98f85047262f1ee
barsuk <barsuk@rg.ru>
2017-08-29 14:10:43 +0300

    add: work with Vue SPA. Please.
    Scheme: v5 at https://goo.gl/U7A9eB

--- www/src/AppBundle/Controller/DefaultController.php
+++ www/src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DefaultController extends Controller
 {
@@ -13,9 +15,17 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        $base_dir = realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR;
+
+        $index_path = $base_dir . 'web/index.html';
+
+        if (!file_exists($index_path)) {
+            throw new NotFoundHttpException('SPA not found.');
+        }
+
+        return new Response(
+            file_get_contents($index_path)
+//            json_encode($resp)
+        );
     }
 }
