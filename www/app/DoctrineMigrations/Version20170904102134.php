164d22a3b0c417b27afc3e3de2ed69842b12eba2
barsuk <barsuk@rg.ru>
2017-09-04 19:27:57 +0300

    add: add the payment by a bank receipt, now with migrations
    Look the scheme-v6 at https://goo.gl/EuVnia

---
+++ www/app/DoctrineMigrations/Version20170904102134.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170904102134 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE vendor (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(200) NOT NULL, address VARCHAR(200) NOT NULL, phone VARCHAR(50) NOT NULL, fax VARCHAR(50) NOT NULL, email VARCHAR(50) NOT NULL, inn VARCHAR(10) NOT NULL, kpp VARCHAR(9) NOT NULL, account VARCHAR(20) NOT NULL, bank VARCHAR(150) NOT NULL, bik VARCHAR(9) NOT NULL, corr VARCHAR(20) NOT NULL, head_name VARCHAR(100) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE vendor');
+    }
+}
