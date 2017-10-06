28ac8219017bba7331e16401ccf5f308c46cd496
barsuk <barsuk@rg.ru>
2017-10-06 13:58:55 +0300

    add: add a description to the Zone table
    Look the scheme-v6 at https://goo.gl/EuVnia

--- www/src/Rg/ApiBundle/Entity/Zone.php
+++ www/src/Rg/ApiBundle/Entity/Zone.php
@@ -177,4 +177,33 @@ class Zone
     {
         return $this->patriffs;
     }
+    /**
+     * @var string
+     */
+    private $description;
+
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Zone
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
 }
