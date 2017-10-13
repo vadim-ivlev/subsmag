8d269e32e677afafb9ca87acc58178e045606213
barsuk <barsuk@rg.ru>
2017-10-13 16:10:42 +0300

    add: output stamp and signs images to the invoice

--- www/src/Rg/ApiBundle/Service/PriceToTextConverter.php
+++ www/src/Rg/ApiBundle/Service/PriceToTextConverter.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copied and refactored from works.rg.ru, MoneyToText
+ * Copied and a bit refactored from works.rg.ru, MoneyToText
  */
 
 namespace Rg\ApiBundle\Service;
@@ -19,31 +19,31 @@ class PriceToTextConverter
     {
         $ret = '';
 
-        $Kop = $dec;
+        $kopeiki = $dec;
 
-        $Rub = $int;
+        $rubli = $int;
 
-        if($Rub)
+        if($rubli)
         {
             // значение $Rub изменяется внутри функции DescrSot
             // новое значение: $Rub %= 1000000000 для миллиарда
-            if($Rub >= 1000000000)
-                $ret .= $this->DescrSot($Rub, 1000000000,
+            if($rubli >= 1000000000)
+                $ret .= $this->textHundreds($rubli, 1000000000,
                         array('миллиард', 'миллиарда', 'миллиардов')) . ' ';
-            if($Rub >= 1000000)
-                $ret .= $this->DescrSot($Rub, 1000000,
+            if($rubli >= 1000000)
+                $ret .= $this->textHundreds($rubli, 1000000,
                         array('миллион', 'миллиона', 'миллионов') ) . ' ';
-            if($Rub >= 1000)
-                $ret .= $this->DescrSot($Rub, 1000,
+            if($rubli >= 1000)
+                $ret .= $this->textHundreds($rubli, 1000,
                         array('тысяча', 'тысячи', 'тысяч'), true) . ' ';
 
-            $ret .= $this->DescrSot($Rub, 1, self::RUB) . ' ';
+            $ret .= $this->textHundreds($rubli, 1, self::RUB) . ' ';
 
             $ret = mb_strtoupper(mb_substr($ret, 0, 1)).mb_substr($ret, 1, mb_strlen($ret));
         }
-        if($Kop < 10)
+        if($kopeiki < 10)
             $ret .= '0';
-        $ret .= $Kop . ' ' . self::KOP[ $this->DescrIdx($Kop) ];
+        $ret .= $kopeiki . ' ' . self::KOP[ $this->descrIdx($kopeiki) ];
 
         return $ret;
     }
@@ -53,14 +53,14 @@ class PriceToTextConverter
     // внутри функции число $in меняется
     // $ar_descr - массив описаний разряда ('миллион', 'миллиона', 'миллионов') и т.д.
     // $fem - признак женского рода разряда числа (true для тысячи)
-    function DescrSot(&$in, $raz, $ar_descr, $fem = false)
+    private function textHundreds(&$in, $raz, $ar_descr, $fem = false)
     {
         $ret = '';
 
         $conv = intval($in / $raz);
         $in %= $raz;
 
-        $descr = $ar_descr[ $this->DescrIdx($conv%100) ];
+        $descr = $ar_descr[ $this->descrIdx($conv%100) ];
 
         if($conv >= 100)
         {
@@ -113,7 +113,7 @@ class PriceToTextConverter
     // ('миллион', 'миллиона', 'миллионов') для числа $ins
     // например для 29 вернется 2 (миллионов)
     // $ins максимум два числа
-    function DescrIdx($ins)
+    private function descrIdx($ins)
     {
         if(intval($ins/10) == 1) // числа 10 - 19: 10 миллионов, 17 миллионов
             return 2;
