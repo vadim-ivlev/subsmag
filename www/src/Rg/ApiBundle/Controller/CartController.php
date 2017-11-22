baa6682ac34afedf40d3cedc4b7b826dc711efe0
barsuk <barsuk@rg.ru>
2017-11-22 18:39:38 +0300

    fix: use cityId set by front
    Look schema-v9 at https://goo.gl/WfjeoJ

--- www/src/Rg/ApiBundle/Controller/CartController.php
+++ www/src/Rg/ApiBundle/Controller/CartController.php
@@ -487,20 +487,7 @@ class CartController extends Controller implements SessionHasCartController
 
     private function getFrontId($request): int
     {
-        //TODO: works only on prod with cookies
-        $rg_geo_data = $request->cookies->get('rg_geo_data') ?? $request->cookies->get('rg_user_region');
-
-        // Test purposes only. Remove!
-        // московская кука
-//        $rg_geo_data = "%7B%22id%22%3A201%2C%22rgId%22%3A3132%2C%22link%22%3A%22%5C%2Fregion%5C%2Fcfo%5C%2Fstolica%5C%2F%22%2C%22originName%22%3A%22%5Cu041c%5Cu043e%5Cu0441%5Cu043a%5Cu0432%5Cu0430%22%2C%22originPrepositionalName%22%3A%22%5Cu041c%5Cu043e%5Cu0441%5Cu043a%5Cu0432%5Cu0435%22%2C%22originGenitiveName%22%3A%22%5Cu041c%5Cu043e%5Cu0441%5Cu043a%5Cu0432%5Cu044b%22%2C%22rubricName%22%3A%22%5Cu041c%5Cu043e%5Cu0441%5Cu043a%5Cu0432%5Cu0430%22%2C%22rubricPrepositionalName%22%3A%22%5Cu041c%5Cu043e%5Cu0441%5Cu043a%5Cu0432%5Cu0435%22%2C%22rubricGenitiveName%22%3A%22%5Cu041c%5Cu043e%5Cu0441%5Cu043a%5Cu0432%5Cu044b%22%7D";
-        // владивостокская кука
-//        $rg_geo_data = "%7B%22id%22%3A96%2C%22rgId%22%3A3718%2C%22link%22%3A%22%5C%2Fregion%5C%2Fdfo%5C%2Fprimorie%5C%2Fvladivostok%5C%2F%22%2C%22originName%22%3A%22%5Cu0412%5Cu043b%5Cu0430%5Cu0434%5Cu0438%5Cu0432%5Cu043e%5Cu0441%5Cu0442%5Cu043e%5Cu043a%22%2C%22originPrepositionalName%22%3A%22%5Cu0412%5Cu043b%5Cu0430%5Cu0434%5Cu0438%5Cu0432%5Cu043e%5Cu0441%5Cu0442%5Cu043e%5Cu043a%5Cu0435%22%2C%22originGenitiveName%22%3A%22%5Cu0412%5Cu043b%5Cu0430%5Cu0434%5Cu0438%5Cu0432%5Cu043e%5Cu0441%5Cu0442%5Cu043e%5Cu043a%5Cu0430%22%2C%22rubricName%22%3A%22%5Cu0412%5Cu043b%5Cu0430%5Cu0434%5Cu0438%5Cu0432%5Cu043e%5Cu0441%5Cu0442%5Cu043e%5Cu043a%22%2C%22rubricPrepositionalName%22%3A%22%5Cu0412%5Cu043b%5Cu0430%5Cu0434%5Cu0438%5Cu0432%5Cu043e%5Cu0441%5Cu0442%5Cu043e%5Cu043a%5Cu0435%22%2C%22rubricGenitiveName%22%3A%22%5Cu0412%5Cu043b%5Cu0430%5Cu0434%5Cu0438%5Cu0432%5Cu043e%5Cu0441%5Cu0442%5Cu043e%5Cu043a%5Cu0430%22%7D";
-
-        if (!$rg_geo_data) {
-            return null;
-        }
-
-        $from_front_id = (json_decode( urldecode($rg_geo_data) ))->id;
+        $from_front_id = $request->cookies->get('cityId') ?? null;
 
         return $from_front_id;
     }
