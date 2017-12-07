900659db964bcb68c046d51f7c7c26415d7e700f
barsuk <barsuk@rg.ru>
2017-12-07 17:01:01 +0300

    fix: add lost migration
    Look schema-v10 at https://goo.gl/WfjeoJ

---
+++ www/app/DoctrineMigrations/Version20171207135518.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171207135518 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE promo ADD is_alert TINYINT(1) NOT NULL, ADD is_visible TINYINT(1) NOT NULL, ADD document VARCHAR(255) DEFAULT NULL, CHANGE image image VARCHAR(255) DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE promo DROP is_alert, DROP is_visible, DROP document, CHANGE image image VARCHAR(255) NOT NULL COLLATE utf8_general_ci');
+    }
+}
