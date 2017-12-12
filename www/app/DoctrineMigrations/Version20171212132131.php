4ed797ccbf6cb17ca069f4b6dce20c33fc90f3aa
barsuk <barsuk@rg.ru>
2017-12-12 16:24:22 +0300

    add: add table for a content
    Look schema-v10 at https://goo.gl/WfjeoJ

---
+++ www/app/DoctrineMigrations/Version20171212132131.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171212132131 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE content (id INT AUTO_INCREMENT NOT NULL, departments LONGTEXT NOT NULL, contacts LONGTEXT NOT NULL, subscribe LONGTEXT NOT NULL, support LONGTEXT NOT NULL, auxiliary LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE content');
+    }
+}
