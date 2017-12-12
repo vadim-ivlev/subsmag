0f99b4057aa106ca1d78759347cc037a4ac53925
barsuk <barsuk@rg.ru>
2017-12-12 13:01:03 +0300

    add: show promo by promo code
    Look schema-v10 at https://goo.gl/WfjeoJ

--- www/src/Rg/ApiBundle/Controller/PromoController.php
+++ www/src/Rg/ApiBundle/Controller/PromoController.php
@@ -13,6 +13,11 @@ use Rg\ApiBundle\Controller\Outer as Out;
  */
 class PromoController extends Controller
 {
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     * @throws \Exception
+     */
     public function listAction(Request $request)
     {
         $promos = $this->getDoctrine()->getRepository('RgApiBundle:Promo')
@@ -40,10 +45,65 @@ class PromoController extends Controller
                     'text2' => $p->getText2(),
                     'text3' => $p->getText3(),
                     'conditions' => $p->getConditions(),
+
+                    'alias' => urlencode($p->getCode()),
                 ];
             },
             $promos
         );
+
+        return (new Out())->json($prepared);
+    }
+
+    /**
+     * @param Request $request
+     * @param string $code
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     * @throws \Exception
+     */
+    public function getOneByCodeAction(Request $request, string $code)
+    {
+        // оригинальный промокод содержит %
+        $decoded = urldecode($code);
+
+        if (
+            strlen($decoded) > 255 or
+            strlen($decoded) < 3 or
+            preg_match('#[^0-9A-Za-z_%-]#', $decoded)
+        ) {
+            $error = 'invalid promocode';
+            return (new Out())->json(['error' => $error]);
+        }
+
+        $p = $this->getDoctrine()->getRepository('RgApiBundle:Promo')
+            ->findOneBy(['code' => $decoded])
+        ;
+        if (is_null($p)) {
+            return (new Out())->json(['error' => 'Акция не найдена или неактивна.']);
+        }
+
+        $prepared = [
+            'name' => $p->getName(),
+//                    'start' => $p->getStart()->format('dd-mm-YY'),
+//                    'end' => $p->getEnd()->format('dd-mm-YY'),
+            'is_active' => $p->getIsActive(),
+            'code' => $p->getCode(),
+            'discount' => $p->getDiscount(),
+            'amount' => $p->getAmount(),
+            'sold' => $p->getSold(),
+            'description' => $p->getDescription(),
+            'image' => $p->getImage(),
+            'is_alert' => $p->getIsAlert(),
+            'is_visible' => $p->getIsVisible(),
+            'is_form' => $p->getIsForm(),
+            'document' => $p->getDocument(),
+            'title' => $p->getTitle(),
+            'text2' => $p->getText2(),
+            'text3' => $p->getText3(),
+            'conditions' => $p->getConditions(),
+
+            'alias' => urlencode($p->getCode()),
+        ];
 
         return (new Out())->json($prepared);
     }
